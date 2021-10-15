ea5b05a3b22767f18b8510addabc042d58c7f2a7
nikolabr <64268040+nikolabr@users.noreply.github.com>
2021-10-15 18:31:24 +0200

    First commit

---
+++ servo.cpp
@@ -0,0 +1,11 @@
+#include "servo.h"
+
+void servo_init(Servo * servo){
+  *(servo->ddr_port) |= (1 << servo->pin);
+}
+
+void servo_write(Servo * servo, unsigned int val){
+   *(servo->port) &= ~(1 << servo->pin); 
+   *(servo->port) |= (val << servo->pin); 
+   delay(500);
+}
---
+++ servo.h
@@ -0,0 +1,17 @@
+#ifndef Servo_h
+#define Servo_h
+
+#define SERVO_DEFINE(a, b) {.ddr_port = &DDR##a, .port = &PORT##a, .pin = b}
+
+#include <Arduino.h>
+
+typedef struct {
+  volatile uint8_t * ddr_port;
+  volatile uint8_t * port;
+  uint8_t pin;
+} Servo;
+
+void servo_init(Servo * servo);
+void servo_write(Servo * servo, unsigned int val);
+
+#endif
