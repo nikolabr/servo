a22309a83a914aa6b66ad128b9b8e6f2eb24d7a7
nikolabr <64268040+nikolabr@users.noreply.github.com>
2021-11-05 20:21:08 +0100

    Fix TCCR1A values for proper interrupts

--- servo_interrupt.ino
+++ servo_interrupt.ino
@@ -14,25 +14,20 @@ void setup() {
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
 
-  //initialize counter value to 0  TCNT1  = 0; 
-
+  //initialize counter value to 0
   TCNT1  = 0; 
 
   // set compare match register for 1hz increments
   // OCR1A = 16*10^6 / (frequency * prescaler) - 1 (result must be <65536)
 
   // 50Hz 
-  OCR1A = 40000;  
+  OCR1A = 5000;  
 
   // 250kHz
   OCR1B = 128;
 
   // turn on CTC mode - timer counter resets to 0
-  TCCR1A |= (1 << WGM12);
   TCCR1B |= (1 << WGM12);
-
-  // Set prescaler for A to 64
-  TCCR1A |= (1 << CS11) | (1 << CS10);  
 
   // Set prescaler for B to 64
   TCCR1B |= (1 << CS11) | (1 << CS10);  
