fea6a0107d3c1efb8ec763ceb90fbc2766df4a68
nikolabr <64268040+nikolabr@users.noreply.github.com>
2021-11-12 20:30:32 +0100

    Decrease prescaler further down to 8

--- servo_interrupt.ino
+++ servo_interrupt.ino
@@ -2,7 +2,7 @@
 
 #define DEFINE_TIMER_VALS(a, b) static uint16_t timer_vals[2] = {a, b - a};
 
-DEFINE_TIMER_VALS(250, 1000)
+DEFINE_TIMER_VALS(250, 8000)
 Servo servo; 
 uint8_t tmp = 1;
 void setup() {
@@ -26,8 +26,8 @@ void setup() {
   // turn on CTC mode - timer counter resets to 0
   TCCR1B |= (1 << WGM12);
 
-  // Set prescaler for B to 64
-  TCCR1B |= (1 << CS11) | (1 << CS10);  
+  // Set prescaler for B to 8
+  TCCR1B |= (1 << CS11);// | (1 << CS10);  
 
   // enable timer compare interrupt for timer TCNT1
   TIMSK1 |= (1 << OCIE1A);
@@ -51,7 +51,7 @@ void loop() {
 //  uint16_t tmp = analogRead(0);    // read the value from the sensor
 //  tmp >>= 2;
 //  tmp |= (1 << 9); // + 256
-  timer_vals[0] = (analogRead(0) >> 2) + 250; // Bit black magic
-  timer_vals[1] = 1000 - timer_vals[0];
+  timer_vals[0] = (analogRead(0) << 1) | (1 << 11); // Bit black magic
+  timer_vals[1] = 8000 - timer_vals[0];
   
 }
